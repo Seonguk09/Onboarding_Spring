a639043c10fe0910bf537e5ca1921cbc29f1cce8
한성욱 <hsw0618@hsw0618-MacBookPro.local>
2026-05-06 19:23:53 +0900

    Feat: API Response 클레스 생성

---
+++ src/main/java/com/example/Onboarding_Spring/config/ApiResponse.java
@@ -0,0 +1,21 @@
+package com.example.Onboarding_Spring.config;
+
+import lombok.Getter;
+
+@Getter
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public ApiResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, message, data);
+    }
+
+}
