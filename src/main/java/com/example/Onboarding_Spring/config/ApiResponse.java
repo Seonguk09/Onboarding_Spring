bda9a8672b9570990eef5539d784491cf197691f
SeongUk09 <hsw0618@midasin.com>
2026-05-07 10:24:09 +0900

    Feat: ApiResponse에 상태 코드 추가

--- src/main/java/com/example/Onboarding_Spring/config/ApiResponse.java
+++ src/main/java/com/example/Onboarding_Spring/config/ApiResponse.java
@@ -4,21 +4,23 @@ import lombok.Getter;
 
 @Getter
 public class ApiResponse<T> {
+    private int code;
     private boolean success;
     private String message;
     private T data;
 
-    public ApiResponse(boolean success, String message, T data) {
+    public ApiResponse(int code, boolean success, String message, T data) {
+        this.code = code;
         this.success = success;
         this.message = message;
         this.data = data;
     }
 
-    public static <T> ApiResponse<T> success(String message, T data) {
-        return new ApiResponse<>(true, message, data);
+    public static <T> ApiResponse<T> success(int code, String message, T data) {
+        return new ApiResponse<>(code, true, message, data);
     }
 
-    public static <T> ApiResponse<T> failure(String message) {
-        return new ApiResponse<>(false, message, null);
+    public static <T> ApiResponse<T> failure(int code, String message) {
+        return new ApiResponse<>(code, false, message, null);
     }
 }
