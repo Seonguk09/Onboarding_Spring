970e6a90ad2a5432a5f32a4495fc91983e3a884c
SeongUk09 <hsw0618@midasin.com>
2026-05-07 16:46:19 +0900

    Refact: Api response class record로 전환

--- src/main/java/com/example/Onboarding_Spring/config/ApiResponse.java
+++ src/main/java/com/example/Onboarding_Spring/config/ApiResponse.java
@@ -1,20 +1,6 @@
 package com.example.Onboarding_Spring.config;
 
-import lombok.Getter;
-
-@Getter
-public class ApiResponse<T> {
-    private final int code;
-    private final boolean success;
-    private final String message;
-    private final T data;
-
-    public ApiResponse(int code, boolean success, String message, T data) {
-        this.code = code;
-        this.success = success;
-        this.message = message;
-        this.data = data;
-    }
+public record ApiResponse<T>(int code, boolean success, String message, T data) {
 
     public static <T> ApiResponse<T> success(int code, String message, T data) {
         return new ApiResponse<>(code, true, message, data);
