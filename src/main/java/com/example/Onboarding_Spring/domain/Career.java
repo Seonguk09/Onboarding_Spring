00a2408b7c3f96900153a8e9fbbfadfb1621bb2d
SeongUk09 <hsw0618@midasin.com>
2026-05-08 09:16:35 +0900

    Feat: 경력 엔티티 구현

---
+++ src/main/java/com/example/Onboarding_Spring/domain/Career.java
@@ -0,0 +1,38 @@
+package com.example.Onboarding_Spring.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "career")
+@Getter @Setter
+public class Career {
+
+    @Id @GeneratedValue
+    private Integer careerId;
+
+    @Column(nullable = false, length = 50)
+    private String company;
+
+    @Column(length = 50)
+    private String role;
+
+    @Column(length = 50)
+    private String team;
+
+    @Column(length = 50)
+    private String position;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private Boolean currentYn;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
+
