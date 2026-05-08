527aeb6070df92e9b4177c93ed6251aee70e7e24
SeongUk09 <hsw0618@midasin.com>
2026-05-08 09:17:54 +0900

    Feat: 공고 경력 중간 엔티티 구현

---
+++ src/main/java/com/example/Onboarding_Spring/domain/JobPostingCareer.java
@@ -0,0 +1,23 @@
+package com.example.Onboarding_Spring.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "job_posting_career")
+@Getter @Setter
+public class JobPostingCareer {
+
+    @Id @GeneratedValue
+    private Integer jobPostingCareerId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "job_posting_id")
+    private JobPosting jobPosting;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "career_id")
+    private Career career;
+}
+
