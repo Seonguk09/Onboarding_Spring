6fb2f2ecb443feafddf474c19285a24746d2fd39
SeongUk09 <hsw0618@midasin.com>
2026-05-08 09:18:09 +0900

    Feat: 공고 학력 중간 엔티티 구현

---
+++ src/main/java/com/example/Onboarding_Spring/domain/JobPostingEducation.java
@@ -0,0 +1,23 @@
+package com.example.Onboarding_Spring.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "job_posting_education")
+@Getter @Setter
+public class JobPostingEducation {
+
+    @Id @GeneratedValue
+    private Integer jobPostingEducationId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "job_posting_id")
+    private JobPosting jobPosting;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "education_id")
+    private Education education;
+}
+
