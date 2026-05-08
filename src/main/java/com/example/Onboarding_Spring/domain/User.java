ebd4e830f3dcd7c36800cc0dbc73d81b1ca5e116
SeongUk09 <hsw0618@midasin.com>
2026-05-08 09:15:49 +0900

    Feat: User 엔티티 구현

---
+++ src/main/java/com/example/Onboarding_Spring/domain/User.java
@@ -0,0 +1,42 @@
+package com.example.Onboarding_Spring.domain;
+
+import com.example.Onboarding_Spring.domain.enums.RoleType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@Getter @Setter
+public class User {
+
+    @Id @GeneratedValue
+    private Integer userId;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 10)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10)
+    private RoleType roleType;
+
+    private LocalDateTime createdDatetime;
+
+    private LocalDateTime modifiedDatetime;
+
+    @OneToMany(mappedBy = "createUser", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Application> applications = new ArrayList<>();
+
+    @OneToMany(mappedBy = "createUser", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<JobPosting> jobPostings = new ArrayList<>();
+}
