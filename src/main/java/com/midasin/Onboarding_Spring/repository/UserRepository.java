69726269a9623dffd59bed397b2f4888e1e83f57
SeongUk09 <hsw0618@midasin.com>
2026-05-10 12:22:26 +0900

    Feat: 회원가입을 위한 유저 조회 기능 레포지토리 구현

---
+++ src/main/java/com/midasin/Onboarding_Spring/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.midasin.Onboarding_Spring.repository;
+
+import com.midasin.Onboarding_Spring.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
