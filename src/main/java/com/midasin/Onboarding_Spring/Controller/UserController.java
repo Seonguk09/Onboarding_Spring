519b68c3fe08d3cf484d94ceac6e5cda70974088
SeongUk09 <hsw0618@midasin.com>
2026-05-10 12:43:09 +0900

    Feat: 어드민 전용 회원가입 api 추가

--- src/main/java/com/midasin/Onboarding_Spring/Controller/UserController.java
+++ src/main/java/com/midasin/Onboarding_Spring/Controller/UserController.java
@@ -1,7 +1,8 @@
 package com.midasin.Onboarding_Spring.controller;
 
 import com.midasin.Onboarding_Spring.common.ApiResponse;
-import com.midasin.Onboarding_Spring.dto.SignUpDto;
+import com.midasin.Onboarding_Spring.dto.AdminSignUpRq;
+import com.midasin.Onboarding_Spring.dto.SignUpRq;
 import com.midasin.Onboarding_Spring.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,8 +17,15 @@ public class UserController {
 
     // 회원가입 API
     @PostMapping("/signUp")
-    public ApiResponse<String> signUp(@RequestBody SignUpDto signUpDto) {
+    public ApiResponse<String> signUp(@RequestBody SignUpRq signUpDto) {
         userService.signUp(signUpDto);
         return ApiResponse.success(201, "회원가입 성공", null);
+    }
+
+    // 어드민 회원가입 API
+    @PostMapping("/admin/signUp")
+    public ApiResponse<String> adminSignUp(@RequestBody AdminSignUpRq adminSignUpRq) {
+        userService.adminSignUp(adminSignUpRq);
+        return ApiResponse.success(201, "어드민 회원가입 성공", null);
     }
 }
