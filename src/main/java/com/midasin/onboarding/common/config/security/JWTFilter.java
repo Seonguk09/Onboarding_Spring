74c7ef06730455568317dcc2bcba8ac80eae90c6
SeongUk09 <hsw0618@midasin.com>
2026-05-11 15:33:24 +0900

    Feat: jwt filter notnull vaild 어노테이션 추가

--- src/main/java/com/midasin/onboarding/common/config/security/JWTFilter.java
+++ src/main/java/com/midasin/onboarding/common/config/security/JWTFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,7 +20,7 @@ public class JWTFilter extends OncePerRequestFilter {
     private final JWTUtil jwtUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         // Authorization 헤더에서 JWT 토큰 추출
