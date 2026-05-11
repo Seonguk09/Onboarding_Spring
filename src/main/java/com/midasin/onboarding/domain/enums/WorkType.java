da136a619fa891b163b36dcaf3efc460041294dd
SeongUk09 <hsw0618@midasin.com>
2026-05-11 18:56:39 +0900

    Feat: worktype 부분 수정

--- src/main/java/com/midasin/onboarding/domain/enums/WorkType.java
+++ src/main/java/com/midasin/onboarding/domain/enums/WorkType.java
@@ -6,8 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum WorkType {
-    FULL_TIME("풀타임"), PART_TIME("파트타임"), INTERN("인턴");
-
+    ONSITE("온사이트"), REMOTE("원격"), HYBRID("하이브리드");
     private final String description;
 }
 
