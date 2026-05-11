1774e18c000fcb72e0fad458c1355315eed4a3d0
SeongUk09 <hsw0618@midasin.com>
2026-05-11 18:57:13 +0900

    Feat: 상태 타입 오타 수정

--- src/main/java/com/midasin/onboarding/domain/enums/StatusType.java
+++ src/main/java/com/midasin/onboarding/domain/enums/StatusType.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum StatusType {
-    OPEN("진헹중"), CLOSED("종료");
+    OPEN("진행중"), CLOSED("종료");
 
     private final String description;
 }
