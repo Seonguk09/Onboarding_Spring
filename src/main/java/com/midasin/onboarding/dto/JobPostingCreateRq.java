e0ae59e5ddef29a387bb61e0b8e2d2c0455126ae
SeongUk09 <hsw0618@midasin.com>
2026-05-11 19:07:06 +0900

    공고 생성 수정시 필수값 설정

--- src/main/java/com/midasin/onboarding/dto/JobPostingCreateRq.java
+++ src/main/java/com/midasin/onboarding/dto/JobPostingCreateRq.java
@@ -2,6 +2,8 @@ package com.midasin.onboarding.dto;
 
 import com.midasin.onboarding.domain.enums.EmploymentType;
 import com.midasin.onboarding.domain.enums.WorkType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record JobPostingCreateRq(String title, String description, String positionName, EmploymentType employmentType, String location, String department, String quantity, String qualification, String requiredCompetency, String preference, String salary, WorkType workType) {
+public record JobPostingCreateRq(@NotBlank String title, String description, String positionName, @NotNull EmploymentType employmentType, String location, String department, String quantity, String qualification, String requiredCompetency, String preference, String salary, @NotNull WorkType workType) {
 }
