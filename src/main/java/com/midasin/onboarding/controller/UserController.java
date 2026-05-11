ed0d16064c51b274ec5c9d39c6dfb215ceae9538
SeongUk09 <hsw0618@midasin.com>
2026-05-11 15:21:50 +0900

    Feat: controller에 valid annotation 추가

--- src/main/java/com/midasin/onboarding/controller/UserController.java
+++ src/main/java/com/midasin/onboarding/controller/UserController.java
@@ -4,6 +4,7 @@ import com.midasin.onboarding.common.ApiResponse;
 import com.midasin.onboarding.dto.AdminSignUpRq;
 import com.midasin.onboarding.dto.SignUpRq;
 import com.midasin.onboarding.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,14 +18,14 @@ public class UserController {
 
     // 회원가입 API
     @PostMapping("/signUp")
-    public ApiResponse<String> signUp(@RequestBody SignUpRq signUpRq) {
+    public ApiResponse<String> signUp(@Valid @RequestBody SignUpRq signUpRq) {
         userService.signUp(signUpRq);
         return ApiResponse.success(201, "회원가입 성공", null);
     }
 
     // 어드민 회원가입 API
     @PostMapping("/admin/signUp")
-    public ApiResponse<String> adminSignUp(@RequestBody AdminSignUpRq adminSignUpRq) {
+    public ApiResponse<String> adminSignUp(@Valid @RequestBody AdminSignUpRq adminSignUpRq) {
         userService.adminSignUp(adminSignUpRq);
         return ApiResponse.success(201, "어드민 회원가입 성공", null);
     }
