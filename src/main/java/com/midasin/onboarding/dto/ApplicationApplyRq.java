14f487932b31c3c8a87e7253488bdec8003660bc
SeongUk09 <hsw0618@midasin.com>
2026-05-12 15:36:43 +0900

    Feat: 지원서 생성 dto 구현

---
+++ src/main/java/com/midasin/onboarding/dto/ApplicationApplyRq.java
@@ -0,0 +1,25 @@
+package com.midasin.onboarding.dto;
+
+import com.midasin.onboarding.domain.enums.ApplicationPathType;
+import com.midasin.onboarding.domain.enums.CurrentType;
+import com.midasin.onboarding.domain.enums.ProficiencyType;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ApplicationApplyRq(@NotNull Integer jobPostingId, String contact, String file, String portfolio,
+                                 ApplicationPathType applicationPathType, List<EducationRq> educations,
+                                 List<CareerRq> careers, List<TechStackRq> techStacks) {
+    public record EducationRq(String school, String major, LocalDateTime startDatetime, LocalDateTime endDatetime,
+                              CurrentType currentType) {
+    }
+
+    public record CareerRq(String company, String role, String team, String position, LocalDate startDate,
+                           LocalDate endDate, Boolean currentYn, String description) {
+    }
+
+    public record TechStackRq(String name, ProficiencyType proficiencyType) {
+    }
+}
