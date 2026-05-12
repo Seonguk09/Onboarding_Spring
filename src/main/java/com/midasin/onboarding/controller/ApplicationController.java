54f712e7c5b942833b5f8a69528a9e7e53841789
SeongUk09 <hsw0618@midasin.com>
2026-05-12 16:34:56 +0900

    Feat: setter 제거에 따른 로직 변경 및 지원서 상태 변경 api 구현

--- src/main/java/com/midasin/onboarding/controller/ApplicationController.java
+++ src/main/java/com/midasin/onboarding/controller/ApplicationController.java
@@ -2,14 +2,11 @@ package com.midasin.onboarding.controller;
 
 import com.midasin.onboarding.common.ApiResponse;
 import com.midasin.onboarding.dto.ApplicationApplyRq;
-import com.midasin.onboarding.dto.ApplicationListRs;
 import com.midasin.onboarding.dto.ApplicationStatusChangeRq;
 import com.midasin.onboarding.service.ApplicationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
