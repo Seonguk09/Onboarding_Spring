c2c6158954679c392062cf8e07e78b712714e9b1
SeongUk09 <hsw0618@midasin.com>
2026-05-12 15:35:58 +0900

    Feat: 기술 스택 레포지토리 구현

---
+++ src/main/java/com/midasin/onboarding/repository/TechStackRepository.java
@@ -0,0 +1,6 @@
+package com.midasin.onboarding.repository;
+
+import com.midasin.onboarding.domain.TechStack;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TechStackRepository extends JpaRepository<TechStack, Integer> {}
